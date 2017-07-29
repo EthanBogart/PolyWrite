550f3a662a3e6c8cf72abe994bc7ebb2dbfc4b17
Ethan Bogart <ethan.bogart@yahoo.com>
2017-07-29 15:07:09 -0400

    Fix adds folder with same name twice bug

--- app/js/reducers/openFolders.js
+++ app/js/reducers/openFolders.js
@@ -3,8 +3,7 @@ import ActionTypes from '../actions/ActionTypes';
 export default function openFolders(folders = [], action) {
   switch (action.type) {
     case ActionTypes.ADD_FOLDER:
-      if (folders.indexOf(action.payload.folder) === -1) {
-        debugger;
+      if (folders.indexOf(action.payload.folder[0]) === -1) {
         return folders.concat(action.payload.folder);
       }
       return folders;
